e2beb068c02c89761186f53b87d0432c9c550808
ruthnot <sean.ruthnot@gmail.com>
2023-02-03 16:26:00 -0800

    binary search in rotated list

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+import requests
+import json
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    url = "https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/"
+    res = requests.get(url)
+    x = res.text
+    print(x)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
