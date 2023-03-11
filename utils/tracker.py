e8f035ad20ca0d687e1478bfcac07bee838ce8a7
ruthnot <sean.ruthnot@gmail.com>
2023-03-10 17:13:43 -0800

    change plot to 52 week based

--- utils/tracker.py
+++ utils/tracker.py
@@ -5,32 +5,40 @@ PROBLEM_REQ = 3
 
 class Tracker(object):
     def __init__(self):
-        self.x_axis = []
-        self.y_axis = []
+        self.x_num, self.x_week = [], []
+        self.y_plan, self.y_actual = [], []
 
     def run(self):
         start = datetime.date(2023, 1, 1)
         for day in range(365):
             date = start + datetime.timedelta(day)
-            if date.weekday() == 0:  # Monday
-                self.x_axis.append(f'{date.month}-{date.day}')
-
-        self.y_axis.append(PROBLEM_REQ)
-        for i in range(1, len(self.x_axis)):
-            self.y_axis.append(self.y_axis[i-1] + PROBLEM_REQ)
+            year, week, weekday = datetime.datetime.isocalendar(date)
+            if year == 2023 and weekday == 7:  # Sunday, the end of week
+                self.x_week.append(f'{date.month}/{date.day}')
+        self.y_plan.append(PROBLEM_REQ)
+        for i in range(1, len(self.x_week)):
+            self.y_plan.append(self.y_plan[i-1] + PROBLEM_REQ)
         self.plot()
 
+    @staticmethod
+    def date_to_num(date):
+        _, week, weekday = datetime.datetime.isocalendar(date)
+        num = week - 1 + round(weekday/7, 1) - 1
+        return num
+
     def plot(self):
-        fig = plt.figure()
-        plt.plot(self.x_axis, self.y_axis, 'r')
+        # Plot Planed Trajectory
+        plt.figure()
+        plt.plot(self.x_week, self.y_plan, 'r')
         plt.xticks(rotation=45)
+
+        # Plot Today's date
+        x_num = Tracker.date_to_num(datetime.datetime.today())
+        plt.axvline(x=x_num)
+
         plt.show()
 
 
-
-
-
-
-
 if __name__=='__main__':
-    Tracker().run()
+    t = Tracker()
+    t.run()
